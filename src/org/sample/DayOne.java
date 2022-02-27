095d12ea50e6e8c3be45c980766c262dd7b5383e
anitha1233 <anithasuresh2305@gmail.com>
2022-02-27 12:56:18 +0530

    username functionality

--- src/org/sample/DayOne.java
+++ src/org/sample/DayOne.java
@@ -8,24 +8,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 
 class DayOne {
-
+		public static void	userName() {
+			System.out.println("username");
+		}
 	public static void main(String[] args) {
 	
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\LENOVO\\eclipse-workspace\\SeleniumDayOne\\Lib\\Driver\\chromedriver.exe");
+	//	System.setProperty("webdriver.chrome.driver","C:\\Users\\LENOVO\\eclipse-workspace\\SeleniumDayOne\\Lib\\Driver\\chromedriver.exe");
 		
-		WebDriver driver=new ChromeDriver();
+	//	WebDriver driver=new ChromeDriver();
 		
-		driver.get("https://www.facebook.com/");
+		userName();
 		
-		WebElement findElement = driver.findElement(By.xpath("//input[@id='email']"));
-		findElement.sendKeys("Greens");
-		
-		WebElement pass= driver.findElement(By.xpath("//input[@id='pass']"));
-		pass.sendKeys("1234");
-		
-		WebElement btn = driver.findElement(By.xpath("//button[@value='1']"));
-		btn.click();
-		
+//		driver.get("https://www.facebook.com/");
+//		
+//		WebElement findElement = driver.findElement(By.xpath("//input[@id='email']"));
+//		findElement.sendKeys("Greens");
+//		
+//		WebElement pass= driver.findElement(By.xpath("//input[@id='pass']"));
+//		pass.sendKeys("1234");
+//		
+//		WebElement btn = driver.findElement(By.xpath("//button[@value='1']"));
+//		btn.click();
+//		
 		
 		
 	}
